a57bb8bbd621f6717cfe6412633a4350bc98ad53
Leonardo Tricotti <tricottileo@gmail.com>
2023-09-23 09:17:25 -0300

    commit

--- src/components/CartNav.js
+++ src/components/CartNav.js
@@ -18,17 +18,23 @@ function CartNav({
       <h3 class="fw-normal text-black mb-5 text-decoration-underline text-center">
         Carrito de compras
       </h3>
-      <nav class="d-flex justify-content-between mb-3 nav-products flex-wrap">
-        <NavButton text="Seguir comprando" onClick={() => continueBuying()} />
-        <NavButton text="Vaciar carrito" onClick={() => deleteAllProducts()} />
-        <NavButton text="Finalizar compra" onClick={() => finishBuy()} />
-      </nav>
-      <nav class="d-flex mb-3 nav-products flex-wrap">
-        <h3 class="fw-normal text-black mb-2">Aún no hay productos</h3>
-        <Link class="btn btn-secondary btn-sm" type="button" to={"/products"}>
-          Seguir comprando
-        </Link>
-      </nav>
+      {cartProducts ? (
+        <nav class="d-flex justify-content-between mb-3 nav-products flex-wrap">
+          <NavButton text="Seguir comprando" onClick={() => continueBuying()} />
+          <NavButton
+            text="Vaciar carrito"
+            onClick={() => deleteAllProducts()}
+          />
+          <NavButton text="Finalizar compra" onClick={() => finishBuy()} />
+        </nav>
+      ) : (
+        <nav class="d-flex mb-3 nav-products flex-wrap">
+          <h3 class="fw-normal text-black mb-2">Aún no hay productos</h3>
+          <Link class="btn btn-secondary btn-sm" type="button" to={"/products"}>
+            Seguir comprando
+          </Link>
+        </nav>
+      )}
     </div>
   );
 }
