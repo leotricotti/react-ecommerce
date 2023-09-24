15eb96469dff91f8f80721e25bd4de899fa0f1c7
Leonardo Tricotti <tricottileo@gmail.com>
2023-09-24 14:16:38 -0300

    commit

--- src/hooks/useGetUserInfo.js
+++
@@ -1,9 +0,0 @@
-export default function useGetUserInfo() {
-  async function getUserInfo() {
-    const response = await fetch("http://localhost:3000/api/sessions/current");
-    const data = await response.json();
-    localStorage.setItem("user", JSON.stringify(data));
-  }
-
-  return getUserInfo;
-}
